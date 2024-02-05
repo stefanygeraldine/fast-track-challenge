ce5aace190d2d852714ebab4579c4441bb7066e7
stefanygeraldine <stefanygeraldine@gmail.com>
2024-02-05 20:41:52 +0100

    firebase setup

---
+++ src/domain/models/users.model.ts
@@ -0,0 +1,7 @@
+export interface IUser {
+  email: string
+  hasCompleteGame: boolean
+}
+export interface IUsersModel {
+  users: IUser[]
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
-import './assets/main.css'
+import '@/infrastructure/assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+import router from './infrastructure/router'
+import { VueFire } from 'vuefire'
+import { firebaseApp } from '@/firebase'
 
 const app = createApp(App)
+
+app.use(VueFire, {
+  // imported above but could also just be created here
+  firebaseApp,
+  modules: []
+})
 
 app.use(createPinia())
 app.use(router)

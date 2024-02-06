de226a253e4f31a984f369b694f6e2fc81e69005
stefanygeraldine <stefanygeraldine@gmail.com>
2024-02-06 07:17:33 +0100

    fix test yml and remove timeout from test

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -1,24 +1,21 @@
 import { test, expect } from '@playwright/test'
 
 test('visits the app root url', async ({ page }) => {
-  test.setTimeout(60000)
   await page.goto('/fast-track-challenge/')
   await expect(page.locator('div > h1')).toHaveText('Are You a Real Pokemon Master')
 })
 
 test('verify the existence of the link "Prove It"', async ({ page }) => {
-  test.setTimeout(60000)
   await page.goto('/fast-track-challenge/')
   const link = page.locator('a[href="/fast-track-challenge/sign-in"]')
   await expect(link).toHaveCount(1)
   await expect(link).toHaveText('Prove It')
 })
 
-test('Go to the sign-in page"', async ({ page }) => {
-  test.setTimeout(60000)
+test('Go to the sign-in page', async ({ page }) => {
   await page.goto('/fast-track-challenge/')
   const link = page.locator('a[href="/fast-track-challenge/sign-in"]')
   await expect(link).toHaveCount(1)
-  await link.click() // In firefox this will fail/timeout.
+  await link.click()
   await expect(page).toHaveURL(/sign-in/)
 })

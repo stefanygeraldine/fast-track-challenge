e11d428ba0985aa3c1ac298f7c9f5742c1dd7a11
stefanygeraldine <stefanygeraldine@gmail.com>
2024-02-08 15:56:16 +0100

    add dependency

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import viteImagemin from 'vite-plugin-imagemin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
 
-    ViteImagemin({
+    viteImagemin({
       gifsicle: {
         optimizationLevel: 7,
         interlaced: false
